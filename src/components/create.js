6d736a01637754696c6f98c736aad3c00959e0bf
MurphyUan <G00385338@gmit.ie>
2021-10-22 17:03:27 +0100

    Lab Upload

--- src/components/create.js
+++ src/components/create.js
@@ -1,13 +1,78 @@
 //Import local Dependencies
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 //Create Class
-class Create extends Component{
+class Create extends Component {
+
+    constructor() {
+        super();
+
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeYear = this.onChangeYear.bind(this);
+        this.onChangePoster = this.onChangePoster.bind(this);
+
+        this.state = {
+            Title: '',
+            Year: '',
+            Poster: ''
+        }
+    }
+    onChangeTitle(e) {
+        this.setState({
+            Title: e.target.value
+        });
+    }
+
+    onChangeYear(e){
+        this.setState({
+            Year: e.target.value
+        });
+    }
+
+    onChangePoster(e){
+        this.setState({
+            Poster: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        alert(`Movie: ${this.state.Title} ~ ${this.state.Year} ~ ${this.state.Poster}`);
+    }
     //Render Method from Component Class
-    render(){
+    render() {
         return (
-            <div>
-                <h3>Hello From Create</h3>
+            <div className='App'>
+                <form onSubmit={this.onSubmit}>
+                    <div className='form-group'>
+                        <label>Add Movie Title: </label>
+                        <input type='text'
+                            className='form-control'
+                            value={this.state.Title}
+                            onChange={this.onChangeTitle}/>
+                    </div>
+                    <div className='form-group'>
+                        <label>Add Movie Year: </label>
+                        <input type='text'
+                            className='form-control'
+                            value={this.state.Year}
+                            onChange={this.onChangeYear}/>
+                    </div>
+                    <div className='form-group'>
+                        <label>Add Movie Poster: </label>
+                        <textarea type='text'
+                            className='form-control'
+                            value={this.state.Poster}
+                            onChange={this.onChangePoster}/>
+                    </div>
+                    <div className='form-group'>
+                        <input type='submit'
+                            className='btn btn-primary'
+                            value='Add Movie' />
+                    </div>
+                </form>
             </div>
         );
     }
--- src/components/read.js
+++ src/components/read.js
@@ -1,4 +1,5 @@
 //Import local Dependencies
+import axios from 'axios';
 import React, { Component } from 'react';
 import Movies from './movies';
 
@@ -6,29 +7,16 @@ import Movies from './movies';
 class Read extends Component {
     //Read State, stores Component Data
     state = {
-        mymovies: [
-            {
-                "Title": "Avengers: Infinity War",
-                "Year": "2018",
-                "imdbID": "tt4154756",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-                "Title": "Captain America: Civil War",
-                "Year": "2016",
-                "imdbID": "tt3498820",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-                "Title": "Charlie Wilson's War",
-                "Year": "2007",
-                "imdbID": "tt0472062",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-        ]
+        mymovies: [ ]
+    };
+
+    componentDidMount(){
+        axios.get('https://jsonblob.com/api/jsonblob/894944504570986496')
+        .then((response) => {
+            this.setState({ mymovies : response.data.movies});
+        }).catch((e) => {
+            console.log(e);
+        });
     }
     //Render method from Component
     render() {
